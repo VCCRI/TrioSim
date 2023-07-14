dce05b9a4f1e5a8c791414b69be75c6e9234976d
Anushi Shah <patel.anushi@gmail.com>
2023-07-14 10:30:31 +1000

    Download path for denovo-db.non-ssc-samples.chr22.UCSC_SNP_INDEL.vcf

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -20,19 +20,22 @@ sudo apt install openjdk-11-jre-headless
 
 sudo apt install tabix
 
-######## bcftools (v 1.7), vcftools (v 0.1.15), Python2, numpy, pandas, pip and pyVCF ############
+######## bcftools (v 1.7), vcftools (v 0.1.15), Python2, pip, numpy, pandas and pyVCF ############
 
 sudo apt install bcftools
 sudo apt install vcftools
 
 sudo add-apt-repository universe
 sudo apt install python2
-sudo apt install python-numpy
-sudo apt install python-pandas
+#sudo apt install python-numpy
+#sudo apt install python-pandas
 
 sudo apt install curl
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 sudo python2 get-pip.py
+
+pip2 install numpy
+pip2 install pandas
 pip2 install PyVCF
 
 
@@ -54,6 +57,7 @@ cd data_files
 
 wget https://vccri-denovo.s3.us-west-2.amazonaws.com/TrioSim/data_files/chromosome_name.txt
 wget https://vccri-denovo.s3.us-west-2.amazonaws.com/TrioSim/data_files/denovo-db.non-ssc-samples.variants_hg38LiftedOver_decoy_hla_UCSC_SNP_INDEL.vcf
+wget https://vccri-denovo.s3.us-west-2.amazonaws.com/TrioSim/data_files/denovo-db.non-ssc-samples.chr22.UCSC_SNP_INDEL.vcf
 wget https://vccri-denovo.s3.us-west-2.amazonaws.com/TrioSim/data_files/chr22_hg38_sites.vcf
 wget https://vccri-denovo.s3.us-west-2.amazonaws.com/TrioSim/data_files/ALL.WGS_GRCh38_sites.20170504.vcf.gz
 wget https://vccri-denovo.s3.us-west-2.amazonaws.com/TrioSim/data_files/ALL.WGS_GRCh38_sites.20170504.vcf.gz.tbi
